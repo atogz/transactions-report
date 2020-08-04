b0f32771eeaead085bc13cc20612e5a4e93ae744
atogz <webdevatogz@gmail.com>
2020-08-04 13:09:55 +0300

    Вывел данные в транзакцию

--- src/components/Transaction.vue
+++ src/components/Transaction.vue
@@ -1,12 +1,12 @@
 <template>
     <details>
         <summary>
-            <div class="order">{{ transactionData.type | operationName }}: <b>89201</b></div>
+            <div class="order">{{ transactionData.type | operationName }}: <b>{{ transactionData.event_id }}</b></div>
             <div class="time"><time datetime="2019-04-18T16:45">16:45</time></div>
             <div class="account">
                 {{ transactionData.account_number }}
             </div>
-            <div class="amount rubles positive">12 380</div>
+            <div class="amount rubles positive">{{ transactionData.amount }}</div>
         </summary>
         <p class="description">{{ transactionData.description ? transactionData.description : "Нет описания операции" }}</p>
     </details>
